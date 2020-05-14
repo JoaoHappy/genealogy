6dedef2b02f3006b9804550af7b7622bd2634a35
João Oliveira <60324712+JoaoHappy@users.noreply.github.com>
2020-05-14 12:41:40 -0300

    1.1

--- arvoreGenealogica.pl
+++ arvoreGenealogica.pl
@@ -32,6 +32,18 @@ mae(heloisa,juca).
 mae(heloisa,vitor).
 mae(heloisa,ronaldo).
 
-irmao(X,Y) :- pai(P,X), pai(P,Y), X\=Y.
-avo(X,Y) :- pai(X,Z), pai(Z,Y).
-bisavo(X,Y,B) :- pai(X,Z),pai(Z,Y),pai(Y,B).
+
+%irmao(X,Y) :- pai(P,X), pai(P,Y), X\=Y.
+
+irmao(Irmao1,Irmao2) :- pai(Pai,Irmao1), pai(Pai,Irmao2),nl.
+
+%avo(X,Y) :- pai(X,Z), pai(Z,Y).
+
+avo(Avo,Neto_a) :- pai(Avo,Neto), pai(Neto,Neto_a),nl.
+
+%bisavo(X,Y,B) :- pai(X,Z),pai(Z,Y),pai(Y,B).
+
+bisavo(Bisavo,Avo,Neta) :- pai(Bisavo,Bisneto), pai(Bisneto,Avo), pai(Avo,Neta),nl.
+
+%tataravo(X,Y,B,T) :- pai(X,Z), pai(Z,Y), pai(Y,B), pai(B,T).
+tataravo(Tataravo,Bisavo,Avo,Tataraneta) :- pai(Tataravo,Tataraneto), pai(Tataraneto,Bisavo), pai(Bisavo,Avo), pai(Avo,Tataraneta),nl.
